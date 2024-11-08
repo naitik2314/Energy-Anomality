808a17417fe66693b755b4b2dde2fa99c6da97e8
Naitik Shah <63101485+naitik2314@users.noreply.github.com>
2024-11-08 13:58:25 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,34 +1,9 @@
-group2_df = spark.sql(f"""
-WITH filtered_spp AS (
-    SELECT DISTINCT BUSINESS_PARTN, START_BB_PERIOD,
-        CASE 
-            WHEN a.DEACTIVATED IS NOT NULL THEN to_date(a.CHANGED_ON, 'yyyy-MM-dd') 
-            ELSE to_date(a.END_BB_PERIOD, 'yyyy-MM-dd') 
-        END AS END_DATE
-    FROM prod.cds_cods.eabp_v AS a
-    WHERE a.PYMT_PLAN_TYPE = 'spp'
-      AND a.START_BB_PERIOD <= '{end_date}'
-      AND (
-          CASE 
-              WHEN a.DEACTIVATED IS NOT NULL THEN to_date(a.CHANGED_ON, 'yyyy-MM-dd') 
-              ELSE to_date(a.END_BB_PERIOD, 'yyyy-MM-dd') 
-          END
-      ) >= '{start_date}'
-),
-
-excluded_bps AS (
-    SELECT DISTINCT `Business Partner` AS BUSINESS_PARTN
-    FROM dev.uncertified.week44_spp_weeklyreminder
-    WHERE `Opt-in Date` >= '{start_date}' AND `Opt-in Date` <= '{end_date}'
-)
-
-SELECT spp.BUSINESS_PARTN, spp.START_BB_PERIOD, spp.END_DATE, dashboard.*
-FROM filtered_spp AS spp
-LEFT ANTI JOIN excluded_bps AS pilot
-ON spp.BUSINESS_PARTN = pilot.BUSINESS_PARTN
-INNER JOIN common.call_volume_dashboard AS dashboard
-ON spp.BUSINESS_PARTN = dashboard.BUSINESS_PARTNER_ID
-  AND spp.START_BB_PERIOD >= dashboard.SEGMENT_START
-WHERE dashboard.START_DATE >= '{start_date}'
-  AND dashboard.END_DATE < '{end_date}'
-""")
+optin_df=spark.sql("""select * from dev.uncertified.week44_spp_weeklyreminder as a 
+                   inner join (select * from common.call_volume_dashboard
+                   WHERE to_date(SEGMENT_START, 'yyyy-MM-dd') >= '{start_date}' 
+                    AND to_date(SEGMENT_STOP, 'yyyy-MM-dd') <= '{end_date}'
+                   ) as b
+                   on a.`Business Partner`=b.BUSINESS_PARTNER_ID 
+                   and date_format(to_timestamp(a.`Opt-In Date`, 'MM/dd/yy hh:mm a'), 'yyyy-MM-dd')>=to_date(b.SEGMENT_START,'yyyy-MM-dd') 
+                   """)
+optin_df.display()
