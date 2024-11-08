740c5cea305b1d1185fdb6c7491783bdf1ae8506
Naitik Shah <63101485+naitik2314@users.noreply.github.com>
2024-11-08 13:59:16 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
-optin_df=spark.sql("""select * from dev.uncertified.week44_spp_weeklyreminder as a 
-                   inner join (select * from common.call_volume_dashboard
-                   WHERE to_date(SEGMENT_START, 'yyyy-MM-dd') >= '{start_date}' 
-                    AND to_date(SEGMENT_STOP, 'yyyy-MM-dd') <= '{end_date}'
-                   ) as b
-                   on a.`Business Partner`=b.BUSINESS_PARTNER_ID 
-                   and date_format(to_timestamp(a.`Opt-In Date`, 'MM/dd/yy hh:mm a'), 'yyyy-MM-dd')>=to_date(b.SEGMENT_START,'yyyy-MM-dd') 
-                   """)
+optin_df = spark.sql(f"""
+SELECT * 
+FROM dev.uncertified.week44_spp_weeklyreminder AS a
+INNER JOIN (
+    SELECT * 
+    FROM common.call_volume_dashboard
+    WHERE to_date(SEGMENT_START, 'yyyy-MM-dd') >= to_date('{start_date}', 'yyyy-MM-dd') 
+      AND to_date(SEGMENT_STOP, 'yyyy-MM-dd') <= to_date('{end_date}', 'yyyy-MM-dd')
+) AS b
+ON a.`Business Partner` = b.BUSINESS_PARTNER_ID 
+   AND date_format(to_timestamp(a.`Opt-In Date`, 'MM/dd/yy hh:mm a'), 'yyyy-MM-dd') >= to_date(b.SEGMENT_START, 'yyyy-MM-dd')
+""")
+
 optin_df.display()
