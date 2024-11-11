8bd2b8efd68d7614169d74033191e71a60619b5d
Naitik Shah <63101485+naitik2314@users.noreply.github.com>
2024-11-11 09:01:28 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,25 @@
-optin_df = spark.sql(f"""
-SELECT a.`Business Partner`, a.`Opt-In Date`, b.SEGMENT_START, b.SEGMENT_STOP, b.NODE_L3, b.HANDLED_TIME
-FROM dev.uncertified.week44_spp_weeklyreminder AS a
-INNER JOIN common.call_volume_dashboard AS b
-ON a.`Business Partner` = b.BUSINESS_PARTNER_ID 
-WHERE date_format(to_timestamp(a.`Opt-In Date`, 'MM/dd/yy hh:mm a'), 'yyyy-MM-dd') >= '{start_date}'
-  AND date_format(to_timestamp(a.`Opt-In Date`, 'MM/dd/yy hh:mm a'), 'yyyy-MM-dd') <= '{end_date}' 
-  AND to_date(b.SEGMENT_START, 'yyyy-MM-dd') >= to_date('{start_date}', 'yyyy-MM-dd') 
-  AND to_date(b.SEGMENT_STOP, 'yyyy-MM-dd') < to_date('{end_date}', 'yyyy-MM-dd')
-  AND b.NODE_L3 = 'SPP ENROLL'
-  AND date_format(to_timestamp(a.`Opt-In Date`, 'MM/dd/yy hh:mm a'), 'yyyy-MM-dd') >= to_date(b.SEGMENT_START, 'yyyy-MM-dd')
+excluded_bps = spark.sql(f"""
+SELECT DISTINCT `Business Partner` AS BUSINESS_PARTN
+FROM dev.uncertified.week44_spp_weeklyreminder
+WHERE date_format(to_timestamp(`Opt-In Date`, 'MM/dd/yy hh:mm a'), 'yyyy-MM-dd') >= '{start_date}'
+  AND date_format(to_timestamp(`Opt-In Date`, 'MM/dd/yy hh:mm a'), 'yyyy-MM-dd') <= '{end_date}'
 """)
-optin_df.display()
+
+group2_df = spark.sql(f"""
+WITH filtered_spp AS (
+    SELECT DISTINCT BUSINESS_PARTN, START_BB_PERIOD,
+        CASE 
+            WHEN DEACTIVATED IS NOT NULL THEN to_date(CHANGED_ON, 'yyyy-MM-dd') 
+            ELSE to_date(END_BB_PERIOD, 'yyyy-MM-dd') 
+        END AS END_DATE
+    FROM prod.cds_cods.eabp_v
+    WHERE PYMT_PLAN_TYPE = 'SPP'
+      AND to_date(START_BB_PERIOD, 'yyyy-MM-dd') <= '{end_date}'
+      AND (CASE WHEN DEACTIVATED IS NOT NULL THEN to_date(CHANGED_ON, 'yyyy-MM-dd') ELSE to_date(END_BB_PERIOD, 'yyyy-MM-dd') END) >= '{start_date}'
+)
+SELECT spp.BUSINESS_PARTN
+FROM filtered_spp AS spp
+LEFT ANTI JOIN excluded_bps AS pilot
+ON spp.BUSINESS_PARTN = pilot.BUSINESS_PARTN
+""")
+group2_df.display()
