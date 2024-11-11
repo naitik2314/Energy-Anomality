745e75b91806391e6a636a38a13256db2a1f004c
Naitik Shah <63101485+naitik2314@users.noreply.github.com>
2024-11-11 08:49:45 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
 
+spark.sql(f"""
+SELECT `Opt-In Date`, date_format(to_timestamp(`Opt-In Date`, 'MM/dd/yy hh:mm a'), 'yyyy-MM-dd') AS OptIn_Date_Parsed
+FROM dev.uncertified.week44_spp_weeklyreminder
+WHERE date_format(to_timestamp(`Opt-In Date`, 'MM/dd/yy hh:mm a'), 'yyyy-MM-dd') >= '{start_date}'
+  AND date_format(to_timestamp(`Opt-In Date`, 'MM/dd/yy hh:mm a'), 'yyyy-MM-dd') <= '{end_date}'
+""").display()
