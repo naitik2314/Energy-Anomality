e1a0e723df6e3479d79c402a57cbfe3b3d85024f
Naitik Shah <63101485+naitik2314@users.noreply.github.com>
2024-11-11 07:47:24 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,34 @@
-# Group 1
-optin_df = spark.sql(f"""
-SELECT * 
-(FROM dev.uncertified.week44_spp_weeklyreminder
-          WHERE date_format(to_timestamp(`Opt-In Date`, 'MM/dd/yy hh:mm a'), 'yyyy-MM-dd') >= '{start_date}'
-          AND date_format(to_timestamp(`Opt-In Date`, 'MM/dd/yy hh:mm a'), 'yyyy-MM-dd') <= '{end_date}' 
- ) AS a
-INNER JOIN (
-    SELECT * 
-    FROM common.call_volume_dashboard
-    WHERE to_date(SEGMENT_START, 'yyyy-MM-dd') >= to_date('{start_date}', 'yyyy-MM-dd') 
-      AND to_date(SEGMENT_STOP, 'yyyy-MM-dd') <= to_date('{end_date}', 'yyyy-MM-dd')
-      AND NODE_L3 = 'SPP ENROLL'
-) AS b
-ON a.`Business Partner` = b.BUSINESS_PARTNER_ID 
-   AND date_format(to_timestamp(a.`Opt-In Date`, 'MM/dd/yy hh:mm a'), 'yyyy-MM-dd') >= to_date(b.SEGMENT_START, 'yyyy-MM-dd')
-""")
-
-print(optin_df.select("Business Partner").distinct().count())
-optin_df.display()
+group3_df = spark.sql(f"""
+WITH combined_bp AS (
+  SELECT DISTINCT BUSINESS_PARTN
+  FROM (
+    SELECT BUSINESS_PARTN,
+      CASE
+        WHEN a.DEACTIVATED IS NOT NULL THEN to_date(a.CHANGED_ON, 'yyyy-MM-dd')
+        ELSE to_date(a.END_BB_PERIOD, 'yyyy-MM-dd')
+      END AS END_DATE
+    FROM prod.cds_cods.eabp_v AS a
+    WHERE a.PYMT_PLAN_TYPE = 'SPP'
+      AND to_date(a.START_BB_PERIOD, 'yyyy-MM-dd') <= '{end_date}'
+      AND (
+          CASE
+            WHEN a.DEACTIVATED IS NOT NULL THEN to_date(a.CHANGED_ON, 'yyyy-MM-dd')
+            ELSE to_date(a.END_BB_PERIOD, 'yyyy-MM-dd')
+          END
+        ) > '{start_date}'
+    UNION
+      SELECT `Business Partner` AS BUSINESS_PARTN,
+        NULL AS END_DATE --Place holder to match column count
+      FROM dev.uncertified.week44_spp_weeklyreminder
+        WHERE date_format(to_timestamp(`Opt-In Date`, 'MM/dd/yy hh:mm a'), 'yyyy-MM-dd') >= '{start_date}'
+        AND date_format(to_timestamp(`Opt-In Date`, 'MM/dd/yy hh:mm a'), 'yyyy-MM-dd') <= '{end_date}'
+  )
+)
+  SELECT dashboard.BUSINESS_PARTNER_ID, dashboard.UCID, dashboard.prorated_call_new, dashboard.NODE_L3, dashboard.HANDLED_TIME
+    FROM common.call_volume_dashboard AS dashboard
+    LEFT ANTI JOIN combined_bp
+    ON dashboard.BUSINESS_PARTNER_ID = combined_bp.BUSINESS_PARTN
+    WHERE to_date(dashboard.SEGMENT_START, 'yyyy-MM-dd') >= '{start_date}' 
+      AND to_date(dashboard.SEGMENT_STOP, 'yyyy-MM-dd') <= '{end_date}'
+      AND dashboard.NODE_L3 = 'SPP ENROLL'
+                      """)
